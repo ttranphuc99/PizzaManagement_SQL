a87d0c8cc06a5d97ddb0d7e998a03e03f4656116
Thien Phuc <ttran.phuc99@gmail.com>
2018-11-12 17:13:56 +0700

    update query

---
+++ Query-official.sql
@@ -0,0 +1,58 @@
+﻿--sản phẩm từng loại được mua nhiều nhất trong tháng 11
+--1. Pizza
+SELECT P.proID, proName, proPrice, sum(detailQuantity) as sumQuantity
+FROM Products P, Orders O, OrderDetail D
+WHERE P.proID = D.proID AND O.orderID = D.orderID AND proType = 'pizza' AND
+	MONTH(createdTime) = 11 AND YEAR(createdTime) = 2018
+GROUP BY P.proID, proName, proPrice
+HAVING sum(detailQuantity) >= all (
+	SELECT sum(detailQuantity)
+	FROM Products P, Orders O, OrderDetail D
+	WHERE P.proID = D.proID AND O.orderID = D.orderID AND proType = 'pizza' AND
+		MONTH(createdTime) = 11 AND YEAR(createdTime) = 2018
+	GROUP BY P.proID, proName, proPrice
+)
+
+--2. Sides
+SELECT P.proID, proName, proPrice, sum(detailQuantity) as sumQuantity
+FROM Products P, Orders O, OrderDetail D
+WHERE P.proID = D.proID AND O.orderID = D.orderID AND proType = 'sides' AND
+	MONTH(createdTime) = 11 AND YEAR(createdTime) = 2018
+GROUP BY P.proID, proName, proPrice
+HAVING sum(detailQuantity) >= all (
+	SELECT sum(detailQuantity)
+	FROM Products P, Orders O, OrderDetail D
+	WHERE P.proID = D.proID AND O.orderID = D.orderID AND proType = 'sides' AND
+		MONTH(createdTime) = 11 AND YEAR(createdTime) = 2018
+	GROUP BY P.proID, proName, proPrice
+)
+
+--3. Drink
+SELECT P.proID, proName, proPrice, sum(detailQuantity) as sumQuantity
+FROM Products P, Orders O, OrderDetail D
+WHERE P.proID = D.proID AND O.orderID = D.orderID AND proType = 'drinks' AND
+	MONTH(createdTime) = 11 AND YEAR(createdTime) = 2018
+GROUP BY P.proID, proName, proPrice
+HAVING sum(detailQuantity) >= all (
+	SELECT sum(detailQuantity)
+	FROM Products P, Orders O, OrderDetail D
+	WHERE P.proID = D.proID AND O.orderID = D.orderID AND proType = 'drinks' AND
+		MONTH(createdTime) = 11 AND YEAR(createdTime) = 2018
+	GROUP BY P.proID, proName, proPrice
+)
+
+--4. Desserts
+SELECT P.proID, proName, proPrice, sum(detailQuantity) as sumQuantity
+FROM Products P, Orders O, OrderDetail D
+WHERE P.proID = D.proID AND O.orderID = D.orderID AND proType = 'desserts' AND
+	MONTH(createdTime) = 11 AND YEAR(createdTime) = 2018
+GROUP BY P.proID, proName, proPrice
+HAVING sum(detailQuantity) >= all (
+	SELECT sum(detailQuantity)
+	FROM Products P, Orders O, OrderDetail D
+	WHERE P.proID = D.proID AND O.orderID = D.orderID AND proType = 'desserts' AND
+		MONTH(createdTime) = 11 AND YEAR(createdTime) = 2018
+	GROUP BY P.proID, proName, proPrice
+)
+
+
